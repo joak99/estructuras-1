befdefda0fb37c1f43c9b77c57dcef85080cbba7
Luis Garreta <lg@simulacion1.eisc.univalle.edu.co>
2017-10-17 09:18:11 -0500

    Version Inicial

---
+++ pila.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+template <class T> 
+class Pila : public vector <T>{
+	public:
+		Pila ();
+		void adicionar (T elem);
+		void mostrar ();
+};
+template <class T>
+Pila <T>::Pila (): vector<T> () {}
+
+template <class T>
+void Pila <T>::adicionar (T elem) {
+	push_back (elem);
+}
+
+template <class T>
+void Pila <T>::mostrar () {
+	int n = this->size ();
+	for (int i=0; i < n; i++)
+		cout << this->at (i)  << ", ";
+	cout << endl;
+}
+
