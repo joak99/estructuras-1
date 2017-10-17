488059258cc7cb160dbba8561e4ef987ca923460
Luis Garreta <lgarreta@gmail.com>
2017-10-17 09:59:00 -0500

    Pilas eliminar

--- mainpila.cpp
+++ mainpila.cpp
@@ -1,6 +1,8 @@
 #include "pila.cpp"
+#include <stdexcept>
 
 int main () {
+  try {
 	Pila <int> p;
 	
 	p.adicionar (2);
@@ -18,4 +20,7 @@ int main () {
 	p.mostrar ();
 	
 	p.eliminar ();
+  }catch (const std::out_of_range &oor) {
+  	cout << "No hay mas elementos en la pila" << endl;
+  }
 }
